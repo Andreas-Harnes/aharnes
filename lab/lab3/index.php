7f3353fed6400734fc98d609d65c0279abcbd027
Andreas <aharnes@csumb.edu>
2017-09-23 04:22:19 +0000

    added lab 3

--- lab/lab3/index.php
+++ lab/lab3/index.php
@@ -1,16 +1,102 @@
 <?php
-    $randomCard = rand(1,13);
+
+
+
+$deck = range(1,41);
+shuffle($deck);
+
+$totalPoints = 0;
+
+function displayHand() {
+    global $deck, $totalPoints;
+    $handPoints = 0;
+    $handAces = 0;
     
-    $suits = array("clubs", "diamonds", "hearts", "spades");
+    for ($i = 0 ; $i < 5 ; $i++) {
+        $lastCard = array_pop($deck);
+        $cardValue = $lastCard % 13;
+        $cardSuit;
+        if($lastCard <= 13) {
+            $cardSuit = "clubs";
+        } else if($lastCard > 13 && $lastCard <= 26) {
+            $cardSuit = "diamonds";
+        } else if($lastCard > 26 && $lastCard <= 39) {
+            $cardSuit = "hearts";
+        } else if($lastCard > 39) {
+            $cardSuit = "spades";
+        }
+        if($cardValue == 0) {
+            $cardValue = 13;
+        }
+        if ($cardValue == 1) { //identifies an ace
+            echo "<img class='ace' src='img/cards/$cardSuit/$cardValue.png' alt='Ace' />";
+            $handAces = $handAces + 1;   //another way to do this is using the syntax: $handAces++;
+        }
+        else {
+            echo "<img src='img/cards/$cardSuit/$cardValue.png' alt='Ace' />";
+        }
+        
+        $handPoints = $handPoints + $cardValue;
+        
+        
+        
+    }
     
-    echo "<img src='img/cards/clubs/$randomCard.png' alt='Clubs' />";
+    echo " Points: " . $handPoints;
+    
+    $totalPoints = $totalPoints + $handPoints;
+    
+    return $handAces;
+}
+
+function identifyWinner($p1, $p2) {
+
+    
+     if ( $p1 > $p2) {
+        echo "Player 1 ";
+    } else if ($p1 < $p2) {
+        echo "Player 2 ";
+    } else {
+        echo "Nobody ";
+    }
+    
+    
+}
+
 ?>
+
 <!DOCTYPE html>
 <html>
     <head>
-        <title> </title>
+        <title> Lab 3: Ace Poker </title>
+        <link href="css/style.css" rel="stylesheet" />
     </head>
     <body>
+        <main>
+            <h1>Ace Poker</h1>
+            <h2>Player with more aces wins all points</h2>
+            
+            <div class="currentCards">
+                <span>You: </span>
+                <?php
+                $player1Aces = displayHand();
+                ?>
+            </div>
+                <br>
+            <div class="currentCards">
+                <span>PC:  </span>
+                <?php
+                $player2Aces = displayHand();
+                ?>
+            </div>
+            
+            <h2>
+            <?=identifyWinner($player1Aces,$player2Aces)?>
+            
+            Wins:
+            <?=$totalPoints?> points!
+            </h2>
+        </main>
 
     </body>
 </html>
---
+++ lab/lab4/index.php
@@ -0,0 +1,46 @@
+<?php
+    $backgroundImage = "img/sea.jpg";
+    if (isset($_GET['keyword'])) {
+                include 'api/pixabayAPI.php';
+                $imageURLs = getImageURLs($_GET['keyword']);
+                $backgroundImage = $imageURLs[array_rand($imageURLs)];
+                
+                for ($i = 0; $i < 5; $i++) {
+                    $currentPic = rand(0, count($imageURLs));
+                    echo "<img src='" . $imageURLs[$currentPic] . "' width = '200' >";
+                    unset($imageURLs[$currentPic]);
+                    array_values($imageURLs);
+                }
+            }
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Lab 4. Pixabay slideshow</title>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <style type="text/css">
+           @import url('css/styles.css'); 
+               body {
+                   background-image: url('<?= $backgroundImage ?>');
+               }
+        </style>
+    </head>
+    <body>
+        <?php
+        
+            if (!isset($imageURLs)) {
+                echo "<h2>Type a keyword to display a slideshow <br /> with random images from pixabay.com</h2>";
+            } else {
+                //thingthingthing
+            }
+        ?>
+
+        <form>
+            <input type="text" name="keyword" placeholder="Keyword"/>
+            <input type="submit" value="Submit" />
+        </form>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </body>
+</html>
